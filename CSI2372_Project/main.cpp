ad23fdc12267936cae8806bdd445134d38a3f4d4
Rami Taleb <rtale089@uottawa.ca>
2017-10-17 08:36:02 -0400

    Initial Commit

---
+++ CSI2372_Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSI2372_Project
+//
+//  Created by Rami Taleb on 2017-10-17.
+//  Copyright © 2017 Rami Taleb. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
